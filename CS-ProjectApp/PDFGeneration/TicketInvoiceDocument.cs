cbef486103d5c573fc27303ff309fa5e38a50426
TruthFire <truth.fire@yandex.ru>
2023-03-05 22:16:46 +0200

    Added qr code to ticket pdf

--- CS-ProjectApp/PDFGeneration/TicketInvoiceDocument.cs
+++ CS-ProjectApp/PDFGeneration/TicketInvoiceDocument.cs
@@ -3,6 +3,7 @@ using QuestPDF.Fluent;
 using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 using CS_ProjectApp.Models;
+using QRCoder;
 
 namespace CS_ProjectApp.PDFGeneration
 {
@@ -47,20 +48,29 @@ namespace CS_ProjectApp.PDFGeneration
 
             container.Row(row =>
             {
-                row.RelativeItem().Column(column =>
+            row.RelativeItem().Column(column =>
+            {
+                column.Item().Text($"Invoice #{Invoice.ticketId}").Style(titleStyle);
+
+                column.Item().Text(text =>
                 {
-                    column.Item().Text($"Invoice #{Invoice.ticketId}").Style(titleStyle);
-
-                    column.Item().Text(text =>
-                    {
-                        text.Span("Issue date: ").SemiBold();
-                        text.Span($"{Invoice.orderTime:d}");
-                    });
-
+                    text.Span("Issue date: ").SemiBold();
+                    text.Span($"{Invoice.orderTime:d}");
                 });
 
-                row.ConstantItem(100).Height(50).Placeholder();
             });
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode($"{Invoice.ticketId}, {Invoice.eventInfo.eventName}", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            qrCode.GetGraphic(10).Save($"./qrcodes/qrc-{Invoice.ticketId}.png");
+
+
+
+            row.ConstantItem(100).Height(100).Image($"./qrcodes/qrc-{Invoice.ticketId}.png", ImageScaling.FitArea);
+            });
+
+            // container.Image("qrc.png");
         }
 
         void ComposeContent(IContainer container)
@@ -71,7 +81,7 @@ namespace CS_ProjectApp.PDFGeneration
                 .Background(Colors.Grey.Lighten3)
                 .AlignCenter()
                 .AlignMiddle()
-                .Text("Content").FontSize(16);
+                .Text($"Event: {Invoice.eventInfo.eventName}\nDate: {Invoice.eventInfo.eventStart}").FontSize(16);
         }
     }
 }
